1003bd12c2e25284b474cf9a3a984d942054bff4
Rohit Shrivastava <rohit.shrivastava93@gmail.com>
2020-08-02 17:33:16 +0530

    v2.1

--- PyClassicRound/PyClassicRound.py
+++ PyClassicRound/PyClassicRound.py
@@ -2,12 +2,5 @@ import decimal
 
 
 def classic_round(num, d_places=2):
-    if not isinstance(num, str):
-        num = str(num)
-        if not isinstance(d_places, int):
-            d_places = int(d_places)
-    d_places_value = '0'
-    if d_places == 0:
-        return int(decimal.Decimal(num).quantize(decimal.Decimal('0'),rounding=decimal.ROUND_HALF_UP))
-    else:
-        return float(decimal.Decimal(num).quantize(decimal.Decimal('0.'+('0'*d_places)),rounding=decimal.ROUND_HALF_UP))
+    if not isinstance(d_places, int): d_places = int(d_places)
+    return int(float(num)-0.5)+1 if d_places == 0 else float(decimal.Decimal(str(num)).quantize(decimal.Decimal('0.'+('0'*d_places)), rounding = decimal.ROUND_HALF_UP))
